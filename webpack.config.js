734fc39b760e6ce7221f914dab63e97a1bf0f8eb
Carolina Vasquez-Ceja <cejac10@gmail.com>
2017-03-23 21:34:43 -0700

    changed webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,70 +1,42 @@
-'use strict';
+const HTMLPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
 
-const dotenv = require('dotenv');
-const webpack = require('webpack');
-const HTMLPlugin = require('html-webpack-plugin');
-const CleanPlugin = require('clean-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-
-const production = process.env.NODE_ENV === 'production';
-
-dotenv.load();
-
-let plugins = [
-  new ExtractTextPlugin('bundle.css'),
-  new HTMLPlugin({ template: `${__dirname}/app/index.html` }),
-  new webpack.DefinePlugin({
-    __API_URL__: JSON.stringify(process.env.API_URL),
-    __DEBUG__: JSON.stringify(!production),
-  }),
-];
-
-if (production) {
-  plugins = plugins.concat([
-    new webpack.optimize.UglifyJsPlugin({
-      mangle: true,
-      compress: {
-        warnings: false,
-      },
-    }),
-    new CleanPlugin(),
-  ]);
-}
+const production = process.env.NODE_ENV === 'production'
 
 module.exports = {
   entry: `${__dirname}/app/entry.js`,
-  devtool: production ? false : 'eval',
-  plugins,
   output: {
-    path: 'build',
     filename: 'bundle.js',
+    path: `${__dirname}/build`
   },
-  sassLoader: {
-    includePaths: [`${__dirname}/app/scss/`],
-  },
+  plugins: [
+    new HTMLPlugin({
+      template: `${__dirname}/app/index.html`
+    }),
+    new webpack.DefinePlugin({
+      __API_URL__: JSON.stringify(process.env.API_URL),
+      __DEBUG__: JSON.stringify(!production)
+    })
+  ],
   module: {
     loaders: [
       {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel',
+        use: 'babel-loader'
       },
       {
         test: /\.html$/,
-        loader: 'html',
-      },
-      {
-        test: /\.(woff|tt|svg|eot).*/,
-        loader: 'url?limit=10000&name=image/[hash].[ext]',
+        use: 'html-loader'
       },
       {
         test: /\.(jpg|jpeg|svg|bmp|tiff|gif|png)$/,
-        loader: 'url?limit=10000&name=image/[hash].[ext]',
-      },
-      {
-        test: /\.scss$/,
-        loader: ExtractTextPlugin.extract('style', 'css!resolve-url!sass?sourceMap'),
-      },
-    ],
+        loader: 'file-loader'
+      }
+    ]
   },
 };
