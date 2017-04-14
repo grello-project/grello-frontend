49fb7dc2fd515b5293dc00a21a30304838a686e9
Jessica Soltero <jmichellevs@gmail.com>
2017-04-14 11:46:25 -0700

    doc filter is working, but dragging tasks is glitchy

--- app/filter/document.js
+++ app/filter/document.js
@@ -2,11 +2,9 @@
 
 module.exports = function() {
   return function(tasks, docID){
-    console.log('RUNNING DOC FILTER', docID)
     if(!docID) return tasks
 
     return tasks.filter(task => {
-      console.log('DO WE GET HERE')
       if (task.document === docID) return task
     })
   }
