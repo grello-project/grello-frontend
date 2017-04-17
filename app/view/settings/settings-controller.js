abe1c63bd6e2ea8dbae0451feb804f37d0b463e3
Jessica Soltero <jmichellevs@gmail.com>
2017-04-17 16:01:52 -0700

    start of settings view

---
+++ app/view/settings/settings-controller.js
@@ -0,0 +1,22 @@
+'use strict'
+
+require('./_settings.scss')
+
+module.exports = ['$location', '$scope', '$log', 'authService', 'taskService', 'categoryService', 'documentService', 'profileService', SettingsController]
+
+function SettingsController ($location, $scope, $log, authService, taskService, categoryService, documentService, profileService) {
+  authService
+    .getToken()
+    .catch( err => {
+      $log.debug(err)
+      $location.url('/')
+    })
+
+  profileService.getUser()
+  .then(user => {
+    this.user = user
+    this.user.profilePic = this.user.profilePic.slice(0, -6)
+    $log.log('THIS IS THE USER FROM SETTINGS VIEW', this.user)
+  })
+  .catch(err => $log.error(err))
+}
